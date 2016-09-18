363ae9fd45fffee57853aaaf599df389f643f909
paulcarroll <pvcarroll@gmail.com>
2016-09-18 09:58:18 -0500

    send new question data to router

--- public/javascripts/addQuestion.js
+++ public/javascripts/addQuestion.js
@@ -1,14 +1,21 @@
 function addAnswer(element) {
-    const newAnswer = document.createElement("div");
-    newAnswer.className = "answer";
-    const answerInput = document.createElement("input");
-    answerInput.type = "text";
-    answerInput.placeholder = "answer text";
-    answerInput.className = "answerInput";
+    const newAnswer = document.createElement('div');
+    newAnswer.className = 'answer';
+    const answerInput = document.createElement('input');
+    answerInput.type = 'text';
+    answerInput.name = 'answerChoice';
+    answerInput.placeholder = 'answer text';
+    answerInput.className = 'answerInput';
     newAnswer.appendChild(answerInput);
     element.parentElement.insertBefore(newAnswer, element);
 }
 
+function addQuestion() {
+    const questionForm = document.forms['add-question'];
+    questionForm.submit();
+}
+
 module.exports = {
-    addAnswer
+    addAnswer,
+    addQuestion
 };
--- routes/addQuestion.js
+++ routes/addQuestion.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('addQuestion', { title: 'Add Survey Question' });
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.send('POST /addQuestion');
+});
+
 module.exports = router;
