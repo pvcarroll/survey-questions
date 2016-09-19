57ccbd582bf86b7972aa916bd3bd3db06f3bf583
paulcarroll <pvcarroll@gmail.com>
2016-09-19 12:11:51 -0500

    save question

--- models/question.js
+++ models/question.js
@@ -4,19 +4,39 @@ var Sequelize = require('sequelize');
 var sequelize;
 var dbConnection;
 
-if (process.env.NODE_ENV) {
-  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
-  dbConnection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-  });
-  dbConnection.connect();
-
-}
-
 function saveQuestion(question, answerChoices) {
+  if (process.env.NODE_ENV) {
+    sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+    dbConnection = mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE
+    });
+    dbConnection.connect();
+
+    const Question = sequelize.define('question', {
+      question: {
+        type: Sequelize.STRING,
+        field: 'question'
+      },
+      answerChoices: {
+        type: Sequelize.ARRAY,
+        field: 'answer_choices'
+      }
+    });
+
+    Question.sync().then(function() {
+      const data = {
+        question: question,
+        answer_choices: answerChoices
+      };
+      Question.create(data).then(function(question) {
+        console.dir(question);
+      });
+    });
+  }
+
   return question + answerChoices;
 }
 
