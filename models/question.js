59432b070b0dedcac8f0887005278401e7182ee5
paulcarroll <pvcarroll@gmail.com>
2016-09-29 10:13:33 -0500

    get random question

--- models/question.js
+++ models/question.js
@@ -2,42 +2,34 @@ var mysql = require('mysql');
 var Sequelize = require('sequelize');
 
 var sequelize;
+if (process.env.CLEARDB_DATABASE_URL) {
+  // Production
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+} else {
+  // Development
+  const localMysqlConfig = require('../config/localMysqlConfig');
+  sequelize = new Sequelize(localMysqlConfig.database, localMysqlConfig.user, localMysqlConfig.password);
+}
+const Question = sequelize.define('question', {
+  text: Sequelize.STRING
+});
+const AnswerChoice = sequelize.define('answer_choice', {
+  answer: Sequelize.STRING
+});
+Question.hasMany(AnswerChoice);
+AnswerChoice.belongsTo(Question, { foreignKey: 'questionId' });
+
+sequelize.sync().then(function() {
+});
 
 function saveQuestion(questionText, answerChoices) {
 
-  if (process.env.CLEARDB_DATABASE_URL) {
-    // Production
-    sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
-  } else {
-    // Development
-    const localMysqlConfig = require("../config/localMysqlConfig");
-    sequelize = new Sequelize(localMysqlConfig.database, localMysqlConfig.user, localMysqlConfig.password);
-  }
-
-  const Question = sequelize.define('question', {
-    text: Sequelize.STRING
-  });
-  const AnswerChoice = sequelize.define('answer_choice', {
-    answer: Sequelize.STRING
-  });
-  Question.hasMany(AnswerChoice);
-  AnswerChoice.belongsTo(Question, { foreignKey: "questionId" });
-
-  sequelize.sync().then(function() {
-  });
-
-  console.log('Question = ' + Question);
-  console.log('AnswerChoice = ' + AnswerChoice);
-
-  var questionId;
-
   Question.sync().then(function() {
-    console.log('Question.sync().then');
     const data = {
       text: questionText
     };
     Question.create(data).then(function(question) {
-      console.dir("QUESTION = " + question);
+      console.dir('QUESTION = ' + question);
 
       AnswerChoice.sync().then(function() {
         var data;
@@ -50,16 +42,30 @@ function saveQuestion(questionText, answerChoices) {
             include: [Question]
           };
           AnswerChoice.create(data, includeObject).then(function(answer_choice) {
-            console.dir("ANSWER_CHOICE = " + answer_choice);
+            console.dir('ANSWER_CHOICE = ' + answer_choice);
           });
         });
       });
     });
   });
-
   return questionText + answerChoices;
 }
 
+function getRandomQuestion() {
+  // sequelize.query('SELECT * FROM questions', { type: sequelize.QueryTypes.SELECT }).then(function(results) {
+  //   console.log("results = " + JSON.stringify(results));
+  // });
+  var randomQuestion = Question.find({
+    order: [
+        Sequelize.fn('RAND')
+    ]
+  }).then(function(result) {
+    console.log('result = ' + JSON.stringify(result));
+  });
+  console.log('randomQuestion = ' + randomQuestion);
+}
+
 module.exports = {
-  saveQuestion: saveQuestion
+  saveQuestion: saveQuestion,
+  getRandomQuestion: getRandomQuestion
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var questionModel = require('../models/question.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  questionModel.getRandomQuestion();
   res.render('index', { title: 'Express' });
 });
 
