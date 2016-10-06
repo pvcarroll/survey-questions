385d9a60cf42b86b55c2adcf8dc84fe3728c958d
paulcarroll <pvcarroll@gmail.com>
2016-10-06 17:21:20 -0500

    production authentication

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ var mysql = require("mysql");
 var Sequelize = require("sequelize");
 var bcrypt = require("bcrypt-nodejs");
 var SALT_FACTOR = 10;
-var PASSWORD = require("../config/passport").password;
+var ADMIN_USERNAME = "admin";
+var ADMIN_PASSWORD = (process.env.ADMIN_PASSWORD) ? process.env.ADMIN_PASSWORD : require("../config/passport").password;
 
 var sequelize;
 if (process.env.CLEARDB_DATABASE_URL) {
@@ -29,13 +30,12 @@ var noop = function() {};
 
 (function createAdminUser() {
   const password = require("../config/passport").password;
-  const username = require("../config/passport").username;
   bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
     if (err) { return done(err); }
     bcrypt.hash(password, salt, noop, function(err, hashedPassword) {
       if (err) { return; }
       // SAVE ADMIN
-      User.build({ username: username, password: hashedPassword }).save().then(function(userResult) {
+      User.build({ username: ADMIN_USERNAME, password: hashedPassword }).save().then(function(userResult) {
         return "User saved";
       }).catch(function(error){});
     });
@@ -43,7 +43,7 @@ var noop = function() {};
 })();
 
 User.checkPassword = function(guess, done) {
-  bcrypt.compare(guess, PASSWORD, function(err, isMatch) {
+  bcrypt.compare(guess, ADMIN_PASSWORD, function(err, isMatch) {
     done(err, isMatch);
   });
 };
